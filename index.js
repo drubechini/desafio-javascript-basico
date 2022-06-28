4c80424891cf045a97144909f4f4598c8161cb64
Deborah Rubechini <drubechini@gmail.com>
2022-06-28 14:32:16 -0300

    adiciona arquivos do desafio js basico

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+function somar(numero1, numero2){
+    let soma = numero1 + numero2
+    console.log('A soma dos dois números é: ' + soma)
+}
+somar(10,2)
+
+
+function subtrair(numero1, numero2){
+    let subtrai = numero1 - numero2
+    console.log('A subtração entre os dois números é: ' + subtrai)
+}
+subtrair(15,7)
+
+
+function dividir(numero1, numero2){
+    let divide = numero1 / numero2
+    console.log('A divisão entre os dois números é: ' + divide)
+}
+dividir(20,10)
+
+
+
+function multiplicar(numero1, numero2){
+    let multiplica = numero1 * numero2
+    console.log('A multiplicação dos dois números é: ' + multiplica)
+}
+multiplicar(8,6)
